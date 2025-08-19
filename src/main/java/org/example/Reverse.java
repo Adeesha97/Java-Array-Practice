343a7aec95da60be7ad142eb31b241a8d639a040
Adeesha97 <adeeshakawshal1997@gmail.com>
2025-08-19 20:25:43 +0530

    add Reverse Method 2

--- src/main/java/org/example/Reverse.java
+++ src/main/java/org/example/Reverse.java
@@ -3,13 +3,17 @@ package org.example;
 public class Reverse {
     public static void main(String[] args){
         String textInput= "ABCD";
-        StringBuilder textOutput= new StringBuilder();
+        StringBuilder textOutput1 = new StringBuilder();
 
         int textInputLength = textInput.length();
 
         for (int i = textInputLength-1; i >=0 ; i--){
-            textOutput.append(textInput.charAt(i));
+            textOutput1.append(textInput.charAt(i));
         }
-        System.out.println("Reverse Array - " + textOutput);
+
+        System.out.println("Reverse Array Method 1 - " + textOutput1);
+
+        StringBuilder textOutput2 = new StringBuilder(textInput);
+        System.out.println("Reverse Array Method 2 - " + textOutput2.reverse());
     }
 }
