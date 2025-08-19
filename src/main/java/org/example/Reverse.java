32bd045d9a6233397bcdad67268edab1c9429fa3
Adeesha97 <adeeshakawshal1997@gmail.com>
2025-08-19 20:01:59 +0530

    add Reverse

--- src/main/java/org/example/Reverse.java
+++ src/main/java/org/example/Reverse.java
@@ -3,12 +3,12 @@ package org.example;
 public class Reverse {
     public static void main(String[] args){
         String textInput= "ABCD";
-        String textOutput= "";
+        StringBuilder textOutput= new StringBuilder();
 
         int textInputLength = textInput.length();
 
         for (int i = textInputLength-1; i >=0 ; i--){
-            textOutput = textOutput + textInput.charAt(i);
+            textOutput.append(textInput.charAt(i));
         }
         System.out.println("Reverse Array - " + textOutput);
     }
